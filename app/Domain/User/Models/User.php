c1d91144e2704355e21f3867905b75afa7f14863
Edcarlo Lima <edcarlo.santos@appmax.com.br>
2025-11-27 08:43:27 -0300

    refactor: Definição do Domain User com migration e factory

---
+++ app/Domain/User/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Domain\User\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * Os atributos que podem ser atribuídos em massa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Os atributos que devem ser ocultados na serialização.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Obtém os atributos que devem ser convertidos.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
